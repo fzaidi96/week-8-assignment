a924274593b5f6b5da624aff1a2eff22c06590aa
fzaidi96 <f.zaidi182@gmail.com>
2024-01-25 15:21:30 +0000

    second commit

--- src/app/urls/blog/page.jsx
+++ src/app/urls/blog/page.jsx
@@ -1,7 +1,18 @@
-export default function Blog() {
+import { sql } from "@vercel/postgres";
+import Link from "next/link";
+
+export default async function Blog() {
+
+  const blogs = await sql`SELECT * FROM blog_posts`
+
     return (
-        <main className="blog">
-        <p>The user will see the list of blogs here</p>
+        <main className="blogpage">
+        <p>The user sees the list of blogs here</p>
+        <div className="blogs">
+          {blogs.rows.map((blog) => {return (<div key={blog.title} className="title">
+         <Link href="/"><h3>{blog.title}</h3></Link></div>)})}
+        </div>
       </main>
     )
 }
+
